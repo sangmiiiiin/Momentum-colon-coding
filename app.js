4876e1a974ae9a84835b8780bc8e55df6473d6bd
SANGMIN <kwk680037@gmail.com>
2023-11-23 23:47:20 +0900

    🚩 Change text when clicking the button again (implemented alone)

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const changeButtonHandler = () => {
     const Text = document.querySelector(".Momentum h1");
-    Text.innerText = "I got you!";
+    if (Text.innerText === "Grab me!") {
+        Text.innerText = "I got you!";
+    } else if (Text.innerText === "I got you!") {
+        Text.innerText = "Grab me!";
+    }
 }
