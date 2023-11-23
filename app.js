cf7545416e752f75b3ae0425c0c47767ba224c71
SANGMIN <kwk680037@gmail.com>
2023-11-24 00:38:25 +0900

    🚩 AddEventListener Change the color of the text using the addEventListener and style methods.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const changeButtonHandler = () => {
-    const Text = document.querySelector(".Momentum h1");
-    if (Text.innerText === "Grab me!") {
-        Text.innerText = "I got you!";
-    } else if (Text.innerText === "I got you!") {
-        Text.innerText = "Grab me!";
-    }
+// const changeButtonHandler = () => {
+//     const Text = document.querySelector(".Momentum h1");
+//     if (Text.innerText === "Grab me!") {
+//         Text.innerText = "I got you!";
+//     } else if (Text.innerText === "I got you!") {
+//         Text.innerText = "Grab me!";
+//     }
+// }
+const title = document.querySelector(".Momentum h1");
+
+function handleTitleClick() {
+    title.style.color = "red";
 }
+
+title.addEventListener("click", handleTitleClick);
