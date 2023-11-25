0635dc8e631b87a52e12f0669928f36673c117ff
SANGMIN <kwk680037@gmail.com>
2023-11-25 10:37:05 +0900

    조건문을 이용해 같은 이벤트에서 다른 반응 구현하기

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
-// const changeButtonHandler = () => {
-//     const Text = document.querySelector(".Momentum h1");
-//     if (Text.innerText === "Grab me!") {
-//         Text.innerText = "I got you!";
-//     } else if (Text.innerText === "I got you!") {
-//         Text.innerText = "Grab me!";
-//     }
-// }
-// const title = document.querySelector(".Momentum h1");
+const clickText = document.querySelector(".container h1");
 
-// console.dir(title);
-
-// function handleTitleClick() {
-//     title.style.color = "red";
-// }
-
-// title.addEventListener("click", handleTitleClick);
-
-const title = document.querySelector(".Momentum h1");
-
-const handleMouseEnter = () => {
-    title.innerText = "Mouse is here!";
+const handleClick = () => {
+	const currentColor = clickText.style.color
+    let changedColor
+    if (currentColor === "red") {
+    	changedColor = "blue"
+    } else {
+    	changedColor = "red"
+    }
+    clickText.style.color = changedColor
 }
-const handleClickEvent = () => {
-    title.style.color = "red";
-}
-
-const handleOnMouse = () => {
-    title.innerText = "Mouse is gon!";
-}
-
-const handleWindowResize = () => {
-    document.body.style.backgroundColor = "tomato";
-}
-
-const handleWindowCopy = () => {
-    alert("Don't copy this");
-}
-
-title.addEventListener("mouseenter", handleMouseEnter);
-// title.addEventListener("click", handleClickEvent);
-title.addEventListener("mouseleave", handleOnMouse);
-title.onclick = handleClickEvent;
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
+clickText.addEventListener("click", handleClick);
