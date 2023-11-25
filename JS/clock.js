d6c34c28df6c3bcc6267f6d3a22a0ff67bab21f1
SANGMIN <kwk680037@gmail.com>
2023-11-25 23:59:38 +0900

    .

--- JS/clock.js
+++ JS/clock.js
@@ -1,8 +1,12 @@
-const clock = document.querySelector("h2#clock");
+const time = document.querySelector("h2#clock");
 
-function getTime() {
+const getTime = () => {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = String(date.getHours()).padStart("2",0);
+    const minutes = String(date.getMinutes()).padStart("2",0);
+    const seconds = String(date.getSeconds()).padStart("2",0);
+    time.innerText = `${hours}:${minutes}:${seconds}`
 }
+
 getTime();
 setInterval(getTime, 1000);
