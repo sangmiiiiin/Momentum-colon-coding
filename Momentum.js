2506caf6295e5b16782530f6af1bb0d79136028b
SANGMIN <kwk680037@gmail.com>
2023-11-25 16:46:19 +0900

    유저이름을 입력하고 제출할시 폼이 사라지게 하고 사용자 이름을 브라우저에 인사말과 출력

--- Momentum.js
+++ Momentum.js
@@ -1,13 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";  // 관습 비교적 중요하지 않은 단순 문자열 같은경우 대문자를 사용하고 스네이크 방식으로 작성
 
 function onLoginSubmit(e) {
     // console.dir(loginInput);
     // console.log("click!!!!!");
-    // const username = loginInput.value;
     e.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+    greeting.innerText = "Hello " + username;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+loginForm.addEventListener("submit", onLoginSubmit);
