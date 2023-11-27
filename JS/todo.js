556f5f01c6f77a6ea09282d199ef177ec56de432
SANGMIN <kwk680037@gmail.com>
2023-11-27 23:41:48 +0900

    ✨ 할일 목록 지우기 버튼 추가

--- JS/todo.js
+++ JS/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoList = document.querySelector("#todo-list");
 const toDoFormInput = toDoForm.querySelector("input");
 
+function deleteToDo(e) {
+    const li = e.target.parentElement;
+    li.remove();
+}
 function paintToDo (newTodo) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
@@ -16,6 +24,10 @@ function handleTodoSubmit (e) {
     const newTodo = toDoFormInput.value;
     toDoFormInput.value = "";
     paintToDo(newTodo);
+}
+
+function handleTodoRemove() {
+    console.log("Click!!")
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
