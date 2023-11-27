bdd7592c637a73fa1f99bfb63857b39ffc53e457
SANGMIN <kwk680037@gmail.com>
2023-11-27 21:29:54 +0900

    🐛 명언과 작가가 일치하지 않는 버그 수정

--- JS/Quotes.js
+++ JS/Quotes.js
@@ -1,30 +1,35 @@
 const Quotes = [
     {
-        quotes: "아니 뗀 굴뚝에 연기나랴?",
+        quote: "아니 뗀 굴뚝에 연기나랴?",
         author: "한국 속담"
     },
     {
-        quotes: "노력은 배신하지 않는다.",
+        quote: "노력은 배신하지 않는다.",
         author: "유명한 명언"
     },
     {
-        quotes: "열번 찍어 안넘어 가는 나무 없다.",
+        quote: "열번 찍어 안넘어 가는 나무 없다.",
         author: "한국 속담"
     },
     {
-        quotes: "가는말이 고와야 오는말이 곱다",
+        quote: "가는말이 고와야 오는말이 곱다",
         author: "한국 속담"
     },
     {
-        quotes: "1% 재능과 99%의 노력",
+        quote: "1% 재능과 99%의 노력",
         author: "작자 미상"
     },
+    {
+        quote: "하면 된다",
+        author: "류상민"
+    }
 ]
 
 const showQuote = document.querySelector("#quotes span:first-child");
 const showAuthor = document.querySelector("#quotes span:last-child");
 
-showQuote.innerText = Quotes[Math.round(Math.random() * 4)].quotes;
-showAuthor.innerText = Quotes[Math.round(Math.random() * 4)].author;
+const todaysQuote = Quotes[Math.round(Math.random() * Quotes.length)];
 
+showQuote.innerText = todaysQuote.quote;
+showAuthor.innerText = todaysQuote.author;
 
