129b11c3eb63508b5da3b4a67bcde84385a1b9ec
SANGMIN <kwk680037@gmail.com>
2023-11-29 23:00:52 +0900

    ✨ 날씨 API를 이용한 현재 위치 날씨 정보 조회 기능 추가

--- JS/weather.js
+++ JS/weather.js
@@ -1,10 +1,22 @@
+const API_KEY = "127938e9c4b3f4145dfb1d41220c0fa6";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url)
+        .then(response => response.json()
+        .then(data => {
+                const weather = document.querySelector("#weather span:first-child");
+                const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        }));
   }
   function onGeoError() {
     alert("Can't find you. No weather for you.");
   }
   
   navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
